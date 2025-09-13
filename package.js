e3f1f4b7e1dc378bc2645deda4c7466175cd7d91
hyranevty <pssitel68@gmail.com>
2025-09-14 02:25:22 +0700

    Add files via upload

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "json-pagination-server",
+  "version": "1.0.0",
+  "description": "Server aktif",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js",
+    "build": "echo 'No build step required'"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "serverless-http": "^3.2.0"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.1"
+  }
+}
